b4679d297ff891b1d72b839071027dadacb1766b
Daniel Mur <daniel.mur@endalia.com>
2020-02-20 20:57:51 +0100

    subida

--- LibraryHashCode/ListBook.cs
+++ LibraryHashCode/ListBook.cs
@@ -2,16 +2,26 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Linq;
 
 namespace LibraryHashCode
 {
     public class ListBook: List<Book>
     {
         public float ScoreSum  =0.0f;
+        public List<int> ids = new List<int>();
         public new void Add(Book item)
         {
-            ScoreSum += item.Score;
-            base.Add(item);
+            if(!ids.Contains(item.Id))
+            {
+                ScoreSum += item.Score;
+                base.Add(item);
+            }
+        }
+
+        public void OrderByScored ()
+        {
+            base.Sort((x, y) => x.Score.CompareTo(y.Score));
         }
     }
 }
